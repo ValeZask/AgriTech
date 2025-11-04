699afb424ad355df0bd6505208bfb97c4ba673eb
Vale <netbakovchyngyz@gmail.com>
2025-11-04 10:50:18 +0600

    Предзагрузка фотографий

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 4096, // мелкие изображения вшиваются в код
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000, immutable'
+    }
+  }
 })
